04b26845bb06dfb413edbff775efb47766690a35
krishnasree1999 <krishnasreemuppa2@gmail.com>
2021-12-30 16:44:58 +0530

    occurance string

---
+++ reverseeachword/reverseeachword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseeachword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+            string strrev = " ";
+
+            foreach (var word in str.Split(' '))
+            {
+                string temp = " ";
+                foreach (var ch in word.ToCharArray())
+                {
+                    temp = ch+ " " +temp;
+                }
+                strrev = strrev +  temp  +  " ";
+            }
+            Console.WriteLine(strrev);
+        }
+    }
+}
---
+++ reverseorderstring/reverseorderstring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseorderstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                int temp;
+                string s = Console.ReadLine();
+                string[] a = s.Split(' ');
+                int k = a.Length - 1;
+                temp = k;
+                for (int i = k; temp >= 0; k--)
+                {
+
+                    Console.Write(a[temp] + "" + ' ');
+                    --temp;
+
+                }
+                    Console.ReadKey();
+
+            }
+        }
+    }
+}
