bc3f80834512b71f5b66816717ed7eafef290225
krishnasree1999 <krishnasreemuppa2@gmail.com>
2021-12-30 16:31:46 +0530

    my second largest prg

---
+++ secondlargest/secondlargest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondlargest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 11, 15, 1, 7, 99, 6, 85, 4 };
+            Array.Sort(array);
+            Array.Reverse(array);
+            Console.WriteLine("Second Highest Value In Array " + array[1]);
+
+            foreach (var result in array)
+            {
+                Console.Write(result + " ");
+            }
+        }
+    }
+}
