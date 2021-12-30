206be3d8b9334b96f76ad032fc07afd7cb9ca419
krishnasree1999 <krishnasreemuppa2@gmail.com>
2021-12-30 16:49:05 +0530

    substring

---
+++ substring/substring/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.Write("Enter a String : ");
+                string inputString = Console.ReadLine();
+
+                Console.WriteLine("All substrings for given string are : ");
+
+                for (int i = 0; i < inputString.Length; ++i)
+                {
+                    StringBuilder subString = new StringBuilder(inputString.Length - i);
+                    for (int j = i; j < inputString.Length; ++j)
+                    {
+                        subString.Append(inputString[j]);
+                        Console.Write(subString + " ");
+                    }
+                }
+                Console.ReadKey();
+        }
+    }
+}
